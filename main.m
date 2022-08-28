504d9212e5fa982a271a542174c75d0667c1072f
SPY <SPY@DESKTOP-AJEPI1G>
2022-08-28 16:58:15 +0800

    新增迭代部分，但是并没能实现随着迭代次数增加，跟踪结果更精确的效果。 具体原因不明正在研究。

--- main.m
+++ main.m
@@ -93,8 +93,9 @@ mat1 = -inv((X.') * Gamma * X + (G.') * Lambda * G);
 mat2 = (X.') * Gamma * (theta * Z_0 + Kec * deltaYr_P - theta_u * u_neg);
 mat3 = (G.') * Lambda * Ut;
 
-W{1, 1} = mat1 * (mat2 - mat3);
-u_0 = W{1, 1}(1);
+W_0 = mat1 * (mat2 - mat3);
+predU_0 = repmat({H * W_0}, iterK, 1);
+u_0 = W_0(1);
 
 %% k = 1, t = 1
 % predictive model
@@ -121,7 +122,7 @@ mat2 = (X.') * Gamma * (theta * Z{1, 1} + Kec * deltaYr_P - theta_u * u_0);
 mat3 = (G.') * Lambda * Ut;
 W{1, 1} = mat1 * (mat2 - mat3);
 
-predU{1, 1} = H * W{1, 1}; 
+predU{1, 1} = H * W{1, 1};
 U(1, 1) = predU{1, 1}(1, 1);
 %% K = 1, t = 2 : T
 for t = 2 : T
@@ -145,12 +146,12 @@ for t = 2 : T
 
     mat2 = (X.') * Gamma * (theta * Z{t, 1} + Kec * deltaYr_P - theta_u * U(t - 1, 1));
     mat3 = (G.') * Lambda * Ut;
-    
+
     W{t, 1} = mat1 * (mat2 - mat3);
     predU{t, 1} = H  * W{t, 1};
     U(t, 1) = predU{t, 1}(1, 1);
 end
-%% assessment of performance
+%% assessment of performance: the first circle
 IAE = zeros(iterK, 1);
 for i = 1 : T
     IAE(1, 1) = IAE(1, 1) + abs(c(i) - Yp(i, 1));
@@ -163,9 +164,89 @@ for i = 1 : T
 end
 IACS(1)
 
+%% k = 2 : iterK
+for k = 2 : iterK
+    % t = 0
+    deltaYr_P = GetDeltaYr(P, w, Yp_0, 15);
+    Ut = [0; zeros(P - 1, 1)];
+    Uk = predU_0{k - 1};
+    mat2 = (X.') * Gamma * (theta * Z_0 + Kec * deltaYr_P - theta_u * u_neg);
+    mat3 = (G.') * Lambda * Ut;
+    mat4 = (H.') * Beta * Uk;
+
+    W_0 = mat1 * (mat2 - mat3- mat4);
+    predU_0{k} = H * W_0;
+    u_0 = predU_0{k}(1);
+
+    % t = 1
+    % predictive model
+    Xm(1, k) = Am * Xm_0 + Bm * u_0;
+    Ym(1, k) = Cm * Xm(1, k);
+    % real process model
+    Xp{1, k} = A * Xp_0 + B * u_0;
+    Yp(1, k) = C * Xp{1, k};
+    delta_xm = Xm(1, k) - Xm_0;
+    et = 0;
+    % extended state vector
+    z = [delta_xm; et];
+    Z{1, k} = z;
+    deltaYr_P = GetDeltaYr(P, w, Yp(1, k), c(1));
+    Ut = [u_0; zeros(P - 1, 1)];
+    Uk = predU{1, k - 1};
+
+    mat2 = (X.') * Gamma * (theta * Z{1, k} + Kec * deltaYr_P - theta_u * u_0);
+    mat3 = (G.') * Lambda * Ut;
+    mat4 = (H.') * Beta * Uk;
+    W{1, k} = mat1 * (mat2 -mat3 - mat4);
+
+    predU{1, k} = H * W{1, k};
+    U(1, k) = predU{1, k}(1);
+
+    for t = 2 : T
+        % predictive model
+        Xm(t, k) = Am * Xm(t - 1, k) + Bm * U(t - 1, k);
+        Ym(t, k) = Cm * Xm(t, k);
+
+        % real process model
+        Xp{t, k} = A * Xp{t - 1, k} + B * U(t - 1, k);
+        Yp(t, k) = C * Xp{t, k};
+
+        delta_xm = Xm(t, k) - Xm(t - 1, k);
+        et = Ym(t, 1) - Ym(t, 1);
+        % extended state vector
+        z = [delta_xm; et];
+        Z{t, k} = z;
+
+        deltaYr_P = GetDeltaYr(P, w, Yp(t, k), c(t));
+
+        Ut = [U(t - 1, k); zeros(P - 1, 1)];
+        Uk = predU{t, k - 1};
+
+        mat2 = (X.') * Gamma * (theta * Z{t, k} + Kec * deltaYr_P - theta_u * U(t - 1, k));
+        mat3 = (G.') * Lambda * Ut;
+        mat4 = (H.') * Beta * Uk;
+        W{t, k} = mat1 * (mat2 -mat3 - mat4);
+        predU{t, k} = H  * W{t, k};
+        U(t, k) = predU{t, k}(1);
+    end
+end
+
+%% assessment of performance: the last circle
+IAE = zeros(iterK, 1);
+for i = 1 : T
+    IAE(1, 1) = IAE(1, 1) + abs(c(i) - Yp(i, iterK));
+end
+IAE(1)
+
+IACS = zeros(iterK, 1);
+for i = 1 : T
+    IACS(1, 1) = IACS(1, 1) + abs(U(i, iterK));
+end
+IACS(1)
+
 %% plot
 plot(1 : T, c, 'r');
 hold on;
-plot(1 : T, Yp(:,1),'b-');
+plot(1 : T, Yp(:,iterK),'b-');
 hold on;
-plot(1 : T, Ym(:,1), 'g+')
+plot(1 : T, Ym(:,iterK), 'g+')
